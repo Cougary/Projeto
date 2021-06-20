a40212e98478138f8083564914f077b2b34d576f
André Luis <fourtinet@gmail.com>
2021-06-20 15:59:09 -0300

    Inserting zip file and a script to return the name of the files inside the zip file and their respective sizes

---
+++ zip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python4
+#!-*-coding:utf-8-*-
+
+import zipfile
+
+arquivo_zip =  input("Digite o arquivo zipado: ")
+zipado = zipfile.ZipFile(arquivo_zip)
+lista = zipado.namelist()
+
+print("Arquivos e diretórios presentes no arquivo zipado: ")
+
+for arquivo in lista:
+	info = zipado.getinfo(arquivo)
+	print("Nome: " + arquivo + " Tamanho: " + str(info.file_size) + " bytes")
+
