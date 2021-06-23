4b355ce89722709a3121c3e96f1f5992f014cc2c
André Luis <fourtinet@gmail.com>
2021-06-23 13:06:43 -0300

    Program that gets prices and names from products in Mercado Livre and prints the data in the screen

---
+++ mercado.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#!-*-coding:utf-8-*-
+
+import requests, bs4
+
+req = requests.get("https://www.mercadolivre.com.br/")
+print(req.raise_for_status)
+arquivo = open("mercado_livre.txt", "w")
+arquivo.write(req.text)
+arquivo.close()
+
+def extrai_dados():
+	sopa = open('mercado_livre.txt')
+	soup = bs4.BeautifulSoup(sopa)
+	filtra_nome = soup.select('.ui-item__title')
+	filtra_preco = soup.select('.price-tag-fraction')
+	for i in range(5):
+		print("Produto: " + str(filtra_nome[i].getText()) + " Preço: " + str(filtra_preco[i].getText()))
+
+extrai_dados()
+
+
+
+
