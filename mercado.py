d27a9cabd95c34e6d1858ae83929bfb3e4b6988b
André Luis <fourtinet@gmail.com>
2021-06-24 08:32:15 -0300

    Inserting comments on mercado.py program

--- mercado.py
+++ mercado.py
@@ -3,12 +3,14 @@
 
 import requests, bs4
 
+# Baixa a página web e salva seu conteúdo no arquivo 'mercado_livre.txt'
 req = requests.get("https://www.mercadolivre.com.br/")
 req.raise_for_status()
 arquivo = open("mercado_livre.txt", "w")
 arquivo.write(req.text)
 arquivo.close()
 
+# Função que extrai e filtra os dados do arquivo baixados para retornar os nomes e preços dos produtos encontrados
 def extrai_dados():
 	sopa = open('mercado_livre.txt')
 	soup = bs4.BeautifulSoup(sopa, features="lxml")
@@ -17,6 +19,7 @@ def extrai_dados():
 	for i in range(5):
 		print("Produto: " + str(filtra_nome[i].getText()) + " Preço: " + str(filtra_preco[i].getText()))
 
+# Chama a função anteriormente definida
 extrai_dados()
 
 
