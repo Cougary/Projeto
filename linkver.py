0636d0e103df9570ada6ff4198bba16b6dec6fea
André Luis <fourtinet@gmail.com>
2021-06-26 07:50:47 -0300

    Adding program linkver.py that takes a given URL and return all links from the URL

---
+++ linkver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#!-*-coding:utf-8-*-
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests, bs4
+
+url = input("Digite o site que deseja acessar: ")
+browser = webdriver.Firefox()
+browser.get(url)
+
+req = requests.get(url)
+req.raise_for_status()
+arquivo = open('link.html', 'wb')
+
+for chunk in req.iter_content(100000):
+	arquivo.write(chunk)
+
+arquivo.close()
+
+arquivo = open('link.html', 'rb')
+sopa = bs4.BeautifulSoup(arquivo.read(), features="lxml")
+elem = sopa.select('a')
+
+for i in range(len(elem)):
+	print("Link: " + elem[i].getText())
