cc1fb246238f9c07b6d7b2f1204cd141d4afac70
André Luis <fourtinet@gmail.com>
2021-06-26 07:52:33 -0300

    Adding comments to linkver.py

--- linkver.py
+++ linkver.py
@@ -5,12 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import requests, bs4
 
+# Acessa o site desejado
 url = input("Digite o site que deseja acessar: ")
 browser = webdriver.Firefox()
 browser.get(url)
 
 req = requests.get(url)
 req.raise_for_status()
+
+# Salva o site baixado em um arquivo HTML
 arquivo = open('link.html', 'wb')
 
 for chunk in req.iter_content(100000):
@@ -18,6 +21,7 @@ for chunk in req.iter_content(100000):
 
 arquivo.close()
 
+# Lê o arquivo HTML e retorna todos os links da página
 arquivo = open('link.html', 'rb')
 sopa = bs4.BeautifulSoup(arquivo.read(), features="lxml")
 elem = sopa.select('a')
