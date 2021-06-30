cf41e0a35c16a211d6e1a40737b43660ff04b882
André Luis <fourtinet@gmail.com>
2021-06-30 09:06:08 -0300

    Inserting comments in excel_dados.py

--- excel_dados.py
+++ excel_dados.py
@@ -3,9 +3,12 @@
 
 import openpyxl
 
+# Carrega o arquivo Excel
 wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb.active
 arquivo = open('dados_planilha.txt', 'w')
+
+# Itera por toda coluna e suas linhas, respectivamente
 for coluna in sheet['A1':'C3']:
     for linha in coluna:
 	    print(linha.value)
