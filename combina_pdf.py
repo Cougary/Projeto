908ce2e6f53cbe7f7d3e97b7a787c401b6f8d398
André Luis <fourtinet@gmail.com>
2021-07-01 11:17:37 -0300

    Inserting comments in combina_pdf.py file

--- combina_pdf.py
+++ combina_pdf.py
@@ -3,6 +3,7 @@
 
 import PyPDF2
 
+# Define os arquivos que serão combinados
 arquivo1 = input("Digite o arquivo com o path absoluto: ")
 arquivo2 = input("Digite outro arquivo com o path absoluto: ")
 pdf1 = open(arquivo1, 'rb')
@@ -11,14 +12,17 @@ pdf1ler = PyPDF2.PdfFileReader(pdf1)
 pdf2ler = PyPDF2.PdfFileReader(pdf2)
 pdfescreve = PyPDF2.PdfFileWriter()
 
+# Itera sobre todas as páginas do arquivo em PDF
 for pagina in range(pdf1ler.numPages):
 	paginaobj = pdf1ler.getPage(pagina)
 	pdfescreve.addPage(paginaobj)
 
+# Itera sobre todas as páginas do arquivo em PDF
 for pagina in range(pdf2ler.numPages):
 	paginaobj = pdf2ler.getPage(pagina)
 	pdfescreve.addPage(paginaobj)
 
+# Escreve as páginas de ambos os arquivos em 'pdf_combinado.pdf'
 pdfsaida = open('pdf_combinado.pdf', 'wb')
 pdfescreve.write(pdfsaida)
 pdfsaida.close()
