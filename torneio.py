652934a6c7014c198d066f1394f1792b1bbe8499
André Luis <fourtinet@gmail.com>
2021-07-12 21:28:50 -0300

    Cleaning the screen

--- torneio.py
+++ torneio.py
@@ -2,6 +2,7 @@
 #!-*-coding:utf-8-*-
 
 import sys 
+import os
 
 print("-" * 18)
 print("CAMPEONATO FUTEBOL")
@@ -11,6 +12,7 @@ time1 = input("Nome do 1o. time: ")
 time2 = input("Nome do 2o. time: ")
 time3 = input("Nome do 3o. time: ")
 
+os.system('clear')
 print("-" * 18)
 print("TABELA DE PARTIDAS")
 print("-" * 18)
