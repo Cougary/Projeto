a290ff1a113c799b9adf784b97ef759693ffb906
André Luis Veras de Oliveira <fourtinet@gmail.com>
2021-08-09 21:50:06 -0300

    Inserting program that takes a user's password in a secure way using getpass module

---
+++ secure_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#!-*-coding:utf-8-*-
+
+import getpass
+
+nome = input("Digite seu nome de usuário: ")
+senha = getpass.getpass("Insira sua senha: ")
+print("Usuário e senha cadastrados")
